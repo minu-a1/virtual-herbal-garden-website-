1203642aafe733c753a7a18b86c1336f75242947
minakshi <minaksh29k@gmail.com>
2024-09-02 20:16:31 +0530

    Detail of 3D WEBSITE

---
+++ navimp - Copy/swing/Word Scramble Game/js/words.js
@@ -0,0 +1,31 @@
+let words = [
+    {
+        word: "BASIL",
+        hint: "PLANTS USE FOR TREATMENT OF COUGF"
+    },
+    {
+        word: "ALOEVERA",
+        hint: "PLANTS USED FOR MOISURISATION"
+    }, 
+    {
+        word: "NEEMS",
+        hint: "PLANTS USED FOR TREATMENT OF WOUNDS"
+    },
+    {
+        word: "TURMERIC",
+        hint: "ROOTS USES AS CANCER TREATMENT"
+    },
+    {
+        word: "ASWAGANDHA",
+        hint: "KING OF AYURVEDA"
+    },
+    {
+        word: "ARJUNA",
+        hint: "PLANTS USED TO TREAT CARDIAC ISSUES"
+    },
+    {
+        word: "BISSU GHASH",
+        hint: "FAMOUS NETTLE PLANT OF UTTRAKHAND PROVIDE SHWERING"
+    },
+    
+]
